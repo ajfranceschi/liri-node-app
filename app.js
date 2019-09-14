17043192a65d65622a6cdbd5a8de216118b20509
Antonio J. Franceschi <antonio.franceschi@icloud.com>
2019-09-13 23:46:38 -0400

    created keys.js file to import keys from .env file. renamed app.js to liri.js liri.js: implemented spotify-this-song case to obtain song details from Spotify and log them. added random.txt file to hold text to be read by app and searched in spotify.

--- app.js
+++
@@ -1,5 +0,0 @@
-const axios = require('axios');
-const sootify = require('node-spotify-api');
-const moment = require('moment');
-const dotenv = require('dotenv');
-
---
+++ liri.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+const SpotifyApi = require('node-spotify-api');
+const moment = require('moment');
+const dotenv = require('dotenv').config();
+const keys = require('./keys');
+const inputArray = process.argv;
+const spotify = new SpotifyApi(keys.spotify);
+
+switch (inputArray[2]) {
+  case 'concert-this':
+    console.log('concert-this');
+    break;
+
+  case 'spotify-this-song':
+    const query = inputArray[3];
+    spotify.search({
+      type: 'track',
+      query: query,
+      limit: 1
+    }).then(data => {
+      const track = data.tracks.items[0];
+      console.log(`Artist(s): ${track.album.artists[0].name}`)
+      console.log(`Song Name: ${track.name}`);
+      console.log(`Preview Link: ${track.external_urls.spotify}`);
+      console.log(`Album Name: ${track.album.name}`);
+    }).catch(error => {
+      console.log(error)
+    });
+    break;
+  default:
+    console.log('Verify your arguments and try again.');
+    break;
+}
+
+
